5ff88081622f2fc40ba176085fccf8953d935eb2
dj.jellings <dj.jellings@collegeamerica.edu>
2016-09-29 12:14:23 -0600

    Parts 1-3 completed.

--- ADOTutorialParts/WebForm2.aspx
+++ ADOTutorialParts/WebForm2.aspx
@@ -8,8 +8,65 @@
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
+    <div> 
+<%--        What is ADO.NET - Part 1
     
+            <asp:GridView ID="GridView1" runat="server" BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2">
+            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+            <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#FFF1D4" />
+            <SortedAscendingHeaderStyle BackColor="#B95C30" />
+            <SortedDescendingCellStyle BackColor="#F1E5CE" />
+            <SortedDescendingHeaderStyle BackColor="#93451F" />
+        </asp:GridView>--%>
+<%--*************************************        SqlConnection in ASP.NET - Part 2              *************************************
+    
+            <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#3366CC" BorderStyle="None" BorderWidth="1px" CellPadding="4">
+            <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />
+            <HeaderStyle BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF" />
+            <PagerStyle BackColor="#99CCCC" ForeColor="#003399" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" ForeColor="#003399" />
+            <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />
+            <SortedAscendingCellStyle BackColor="#EDF6F6" />
+            <SortedAscendingHeaderStyle BackColor="#0D4AC4" />
+            <SortedDescendingCellStyle BackColor="#D6DFDF" />
+            <SortedDescendingHeaderStyle BackColor="#002876" />
+        </asp:GridView>--%>
+<%-- ******************            ConnectionStrings in web.config configuration file - Part 3           *************        
+    
+    <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+            <SortedAscendingCellStyle BackColor="#FDF5AC" />
+            <SortedAscendingHeaderStyle BackColor="#4D0000" />
+            <SortedDescendingCellStyle BackColor="#FCF6C0" />
+            <SortedDescendingHeaderStyle BackColor="#820000" />
+        </asp:GridView>--%>
+        <%--SqlCommand in ADO.NET Part 4--%>
+
+
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+
+
     </div>
     </form>
 </body>
--- ADOTutorialParts/WebForm2.aspx.cs
+++ ADOTutorialParts/WebForm2.aspx.cs
@@ -4,14 +4,119 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.OracleClient;
+using System.Configuration;
 
 namespace ADOTutorialParts
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
+        /* What is ADO.NET - Part 1
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+
+          //  *Same setup but different Classes for SqlClient, Odbc, OleDb, and OracleClient.
+
+          // *
+          // *
+          // *
+          // *
+          // *SQL Connection Way
+          // *
+          //SqlConnection con = new SqlConnection("data source=.; database=Sample; integrated security=SSPI");
+          //SqlCommand cmd = new SqlCommand("select * from tblProduct", con);
+          //  con.Open();
+          //  SqlDataReader rdr = cmd.ExecuteReader();
+          //  GridView1.DataSource = rdr;
+          //  GridView1.DataBind();
+          //  con.Close();
+          //  *
+          //  /* 
+          //   * 
+          //   * Oracle Connection Way. Has been deprecated and is obsolete.
+          //   *
+          //  OracleConnection con = new OracleConnection("data source=.; database=Sample; integrated security=SSPI");
+          //  OracleCommand cmd = new OracleCommand("select * from tblProduct", con);
+          //  con.Open();
+          //  OracleDataReader rdr = cmd.ExecuteReader();
+          //  GridView1.DataSource = rdr;
+          //  GridView1.DataBind();
+          //  con.Close();
+            
+
+        }*/
+        /*SqlConnection in ASP.NET - Part 2*
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //initial catalog or database will work for connecting the database.
+            //windows or server authentication. Windows - integrated security. Server - User id =something ; password = something.
+            string CS = "data source=.; database=Sample; integrated security=SSPI";
+            using (SqlConnection con = new SqlConnection(CS))//Interview question: How are using statements used.
+                                                            //When using a namespace or closing a connection properly.
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+
+            //try
+            //{
+            //    SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+            //    con.Open();
+            //    GridView1.DataSource = cmd.ExecuteReader();
+            //    GridView1.DataBind();
+            //}
+            //catch
+            //{
+
+            //}
+            //finally
+            //{
+            //    con.Close();
+            //}
+            }
+            */
+        /* ConnectionStrings in web.config configuration file - Part 3
+         * 
+         * 
+         * Don't put connection strings into the code, hard coded style.
+         * Causing rebuild and redeploy and it will take too much time.
+         * 
+         *
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))//Interview question: How are using statements used.
+                                                             //When using a namespace or closing a connection properly.
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+        }
+        */
+        /* SqlCommand in ADO.NET Part 4
+         * 
+         * 
+         */
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
         }
     }
 }
